c8c3eee1bc9e762eabe31f75d787f49dd2eea1ac
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-10-19 22:58:09 +0530

    Enhanced Key In Hash Table Implementation.

--- 4_HashTableImplement.js
+++ 4_HashTableImplement.js
@@ -37,16 +37,39 @@ class HashTable {
     return undefined;
   }
 
+  // keys() {
+  //   const keysArray = [];
+  //   // console.log(this.data.length);
+  //   for (let i = 0; i < this.data.length; i++) {
+  //     if (this.data[i]) {
+  //       // console.log(this.data[i][0]);
+  //       keysArray.push(this.data[i][0][0]);
+  //     }
+  //   }
+  //   return keysArray;
+  // }
+
+  // Enhanced Key Function.
   keys() {
-    const keysArray = [];
-    // console.log(this.data.length);
+    if (!this.data.length) {
+      return undefined;
+    }
+    let result = [];
+    // loop through all the elements
     for (let i = 0; i < this.data.length; i++) {
-      if (this.data[i]) {
-        // console.log(this.data[i][0]);
-        keysArray.push(this.data[i][0][0]);
+      // if it's not an empty memory cell
+      if (this.data[i] && this.data[i].length) {
+        // but also loop through all the potential collisions
+        if (this.data.length > 1) {
+          for (let j = 0; j < this.data[i].length; j++) {
+            result.push(this.data[i][j][0]);
+          }
+        } else {
+          result.push(this.data[i][0]);
+        }
       }
     }
-    return keysArray;
+    return result;
   }
 }
 
