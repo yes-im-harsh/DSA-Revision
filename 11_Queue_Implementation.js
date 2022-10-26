dbb368b707ed538d4b1cae9751cf6d808c058d95
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-10-26 23:52:00 +0530

    Queue Implementation

---
+++ 11_Queue_Implementation.js
@@ -0,0 +1,61 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+
+  peek() {
+    return this.first;
+  }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+
+    if (this.length === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    }
+
+    this.last.next = newNode;
+    this.last = newNode;
+    this.length++;
+    return this;
+  }
+
+  dequeue() {
+    if (!this.first) {
+      return null;
+    }
+
+    if (this.first === this.last) {
+      this.last = null;
+    }
+
+    this.first = this.first.next;
+    this.length--;
+    return this;
+  }
+
+  isEmpty() {
+    this.length === 0 ? true : false;
+  }
+}
+
+const myQueue = new Queue();
+console.log(myQueue.enqueue("Joy"));
+console.log(myQueue.enqueue("matt"));
+console.log(myQueue.enqueue("Pavel"));
+console.log(myQueue.enqueue("Samir"));
+console.log(myQueue.peek());
+console.log(myQueue.dequeue());
+console.log(myQueue.dequeue());
+console.log(myQueue.dequeue());
+console.log(myQueue.dequeue());
